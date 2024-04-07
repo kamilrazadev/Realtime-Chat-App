fa7206a6f1b8c3d9340159507c524f43628c2642
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-04-08 02:46:57 +0500

    custom scroll ba made

--- src/components/sidebar/SearchBar.jsx
+++ src/components/sidebar/SearchBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const SearchBar = () => {
   return (
-    <div className="flex gap-2 px-2 py-4 bg-gray-500">
+    <div className="flex gap-2 px-2 py-4 border-b border-slate-600">
       <input
         type="text"
         placeholder="Search..."
