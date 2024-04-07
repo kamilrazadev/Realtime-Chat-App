2bdd057c4e01b5d646fbbd1b5328348ad078f569
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-04-07 15:58:47 +0500

    frontend default code removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { serverUrl } from "./appConstants";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { axios } from "axios";
 
 const App = () => {
-  return <div>Home</div>;
+  const [data, setData] = useState("");
+  const fetchData = async () => {
+    const response = await axios.get(serverUrl);
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>App</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
