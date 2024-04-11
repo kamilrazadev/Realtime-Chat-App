e9fb781f18bc14ca151db2776b3a94100c956a3b
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-04-11 21:44:48 +0500

    local server url added

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,2 +1,2 @@
-// export const serverUrl = "http://localhost:5000";
-export const serverUrl = "https://mychatapp-server.vercel.app";
+    export const serverUrl = "http://localhost:5000";
+// export const serverUrl = "https://mychatapp-server.vercel.app";
