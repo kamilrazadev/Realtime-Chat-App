be485a66496a60b861cf32b039837ac33c06714c
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-04-11 20:33:32 +0500

    fully responsive chats

--- src/components/chatarea/ConversationHeader.jsx
+++ src/components/chatarea/ConversationHeader.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { IoArrowBackOutline } from "react-icons/io5";
+
 import useConversation from "../../zustand/useConversation";
 import { useSocketContext } from "../../context/SocketContext";
 
 const ConversationHeader = () => {
-  const { selectedConversation } = useConversation();
+  const { selectedConversation, setSelectedConversation } = useConversation();
   const { onlineUsers } = useSocketContext();
 
   const isThisUserOnline = onlineUsers.includes(selectedConversation?._id);
 
   return (
     <div className=" flex gap-4 p-2 backdrop-blur-sm">
-      <div className="avatar">
-        <div className="w-14 rounded-full">
-          <img src={selectedConversation?.profilePic} />
+      <div className="flex items-center gap-1">
+        <div
+          className="flex items-center"
+          onClick={() => setSelectedConversation(null)}
+        >
+          <IoArrowBackOutline className="text-2xl" />
+        </div>
+        <div className="avatar">
+          <div className="w-14 rounded-full">
+            <img src={selectedConversation?.profilePic} />
+          </div>
         </div>
       </div>
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
 import ChatArea from "../../components/chatarea/ChatArea";
+import useConversation from "../../zustand/useConversation";
 
 const Home = () => {
+  const { selectedConversation } = useConversation();
+
   return (
     <div className="h-screen w-full flex">
-      <div className="flex flex-col">
+      <div className="flex flex-col max-[660px]:w-full w-fit">
         <Sidebar />
       </div>
-      <div className="flex-1 h-screen flex">
+      <div
+        className={`flex-1 h-screen flex ${
+          selectedConversation
+            ? "max-[660px]:fixed max-[660px]:top-0 max-[660px]:left-0 max-[660px]:z-50 max-[660px]:w-full"
+            : "max-[660px]:hidden"
+        }`}
+      >
         <ChatArea />
       </div>
     </div>
