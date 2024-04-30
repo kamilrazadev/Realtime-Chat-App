3b0e717d87d69abe51695a8b51179216c8ab9ab5
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-04-30 15:09:30 +0500

    final input UI

--- src/components/chatarea/FormInput.jsx
+++ src/components/chatarea/FormInput.jsx
@@ -13,22 +13,49 @@ const FormInput = () => {
   };
 
   return (
-    <form
-      className="w-full flex gap-2 bg-[#1D232A] p-2 absolute bottom-0 left-0"
-      onSubmit={handleSendMessage}
-    >
-      <input
-        id="message"
-        value={message}
-        type="text"
-        placeholder="Send a message..."
-        className="outline-none border-none bg-transparent flex-1 input-sm"
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button type="submit" className="btn btn-neutral btn-sm">
-        {loading ? <span className="loading loading-spinner "></span> : "Send"}
-      </button>
-    </form>
+    <>
+      <form
+        className="max-[660px]:hidden flex w-full gap-2 bg-[#1D232A] p-2 absolute bottom-0 left-0"
+        onSubmit={handleSendMessage}
+      >
+        <input
+          id="message"
+          value={message}
+          type="text"
+          placeholder="Send a message..."
+          className="outline-none border-none bg-transparent flex-1 input-sm"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit" className="btn btn-neutral btn-sm">
+          {loading ? (
+            <span className="loading loading-spinner "></span>
+          ) : (
+            "Send"
+          )}
+        </button>
+      </form>
+
+      <form
+        className="max-[660px]:flex hidden w-full gap-2 bg-[#1D232A] p-2 fixed bottom-0 left-0"
+        onSubmit={handleSendMessage}
+      >
+        <input
+          id="message"
+          value={message}
+          type="text"
+          placeholder="Send a message..."
+          className="outline-none border-none bg-transparent flex-1 input-sm"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <button type="submit" className="btn btn-neutral btn-sm">
+          {loading ? (
+            <span className="loading loading-spinner "></span>
+          ) : (
+            "Send"
+          )}
+        </button>
+      </form>
+    </>
   );
 };
 
